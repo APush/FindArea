d1d2694a748e101b829a7852c3718053ee1676a5
APush <pushkin.eng@gmail.com>
2015-08-24 16:22:06 +0300

    Create FindArea.cs

---
+++ FindArea.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindArea
+{
+    public class FindAreaR
+    {
+        
+        public static double RectArea(double x, double y)
+        {
+            double Area = x * y;
+            return Area;
+        }
+    }
+}
